e9439090e0f342c4dbaef0d2dc915daf459719b8
Phamtung04 <phamtunghy1234@gmail.com>
2025-03-24 17:08:44 +0700

    change gitignore

--- assignment-training-reactjs/src/router/AuthRoute.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
-
-const AuthRoute = () => {
-  const token = localStorage.getItem('token');
-  return token ? <Navigate to="/users" replace /> : <Outlet />;
-};
-
-export default AuthRoute;
