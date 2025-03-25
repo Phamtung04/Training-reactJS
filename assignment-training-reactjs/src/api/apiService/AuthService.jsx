cefd6de59df93f05c9b7a4e26b3df2cd64597118
Phamtung04 <phamtunghy1234@gmail.com>
2025-03-25 14:38:13 +0700

    Sửa lại tên file, sửa một số lỗi trong code

--- assignment-training-reactjs/src/api/apiService/AuthService.jsx
+++
@@ -1,9 +0,0 @@
-import Api from '../Api';
-import API_PATH from '../../constants/ApiPath';
-
-export const AuthService = {
-  register: (data) => Api.post(API_PATH.AUTH.REGISTER, data),
-  login: (data) => Api.post(API_PATH.AUTH.LOGIN, data),
-  forgotPassword: (data) => Api.post(API_PATH.AUTH.FORGOT_PASSWORD, data),
-  confirmPassword: (data) => Api.post(API_PATH.AUTH.CONFIRM_PASSWORD, data),
-};
--- assignment-training-reactjs/src/hooks/UseUserToken.js
+++
@@ -1,8 +0,0 @@
-import { jwtDecode } from "jwt-decode";
-
-export const UseUserToken = () => {
-  const token = localStorage.getItem('token');
-  const decoded = jwtDecode(token);
-
-  return {decoded};
-};
